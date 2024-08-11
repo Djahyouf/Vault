1b9dfc56de548b8a72054f7e91578926386f2dc9
leo habets <leo.habets@epita.fr>
2024-08-11 17:00:43 +0200

    organization
    less code in main :
     - no functions
     - just modules
     - just main function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::io::{self, Write};
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+use std::str;
 
 #[path = "data/data_block.rs"]
 mod data_block;
@@ -14,47 +12,15 @@ mod keys;
 mod nonce;
 #[path = "encryption/salt.rs"]
 mod salt;
-
-fn ask_for_master_pswd() -> String {
-    print!("Enter master password : ");
-    io::stdout().flush().unwrap();
-
-    let stdin = io::stdin();
-    let mut stdout = io::stdout().into_raw_mode().unwrap();
-
-    let mut password = String::new();
-
-    for c in stdin.keys() {
-        match c.unwrap() {
-            termion::event::Key::Char('\n') => break,
-            termion::event::Key::Char(c) => {
-                password.push(c);
-                write!(stdout, "*").unwrap();
-                stdout.flush().unwrap();
-            }
-            termion::event::Key::Backspace => {
-                if !password.is_empty() {
-                    password.pop();
-                    write!(stdout, "\u{8} \u{8}").unwrap();
-                    stdout.flush().unwrap();
-                }
-            }
-            _ => {}
-        }
-    }
-
-    write!(stdout, "\n\r").unwrap();
-    stdout.flush().unwrap();
-
-    password
-}
+mod utils;
 
 fn main() {
-    let master_password = ask_for_master_pswd();
+    let master_password = utils::ask_for_master_pswd();
     println!("You entered the following : {}", master_password);
 
     let plaintext = b"plaintext message";
     println!("Plaintext: {:?}", plaintext);
+    println!("Plaintext: {}", str::from_utf8(plaintext).unwrap());
 
     let (ciphertext, salt, nonce) = encryption::encrypt(plaintext, &master_password);
     println!("Ciphertext: {:?}", ciphertext);
